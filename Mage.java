a894c3ad8af12b275633db750e183a95aa10d69c
Swayam-The Coder <swayamcode1@gmail.com>
2026-08-27 14:19:57 +0530

    ...

--- Mage.java
+++ Mage.java
@@ -1,4 +1,15 @@
 package OOPS.Projects.GameCharacterSystem;
 
-public class Mage {
+public class Mage extends Character{
+    private String spell;
+
+    Mage(String name, int health, int level, String spell){
+        super(name, health, level);
+        this.spell = spell;
+    }
+
+    @Override
+    public void attack() {
+        System.out.println(getName()+ " attacks using "+spell);
+    }
 }
--- main.java
+++ main.java
@@ -7,6 +7,7 @@ public class main {
     public static void main(String[] args) {
         Character warrior = new Warrior("Thor", 500, 4, "Hammer");
         Character archer = new Archer("Super Archer", 200, 7, "Frozen Arrow");
+        Character mage = new Mage("Wizard", 300, 9, "Poison Spell");
 
 
         System.out.println("----WARRIOR----");
@@ -21,7 +22,25 @@ public class main {
         archer.attack();
         archer.takeDamage(50);
 
+        System.out.println();
 
+        System.out.println("----Mage----");
+        mage.showDetails();
+        mage.attack();
+        mage.takeDamage(40);
+
+        System.out.println();
+        System.out.println("--- ALL CHARACTERS ATTACK ---");
+
+        Character[] characters = {
+                new Warrior("Thor",100,5,"Sword"),
+                new Mage("Merlin", 80,6,"Fireball"),
+                new Archer("Robin",90,4,"Poison Arrow")
+        };
+
+        for(Character ch: characters){
+            ch.attack();
+        }
 
     }
 }
